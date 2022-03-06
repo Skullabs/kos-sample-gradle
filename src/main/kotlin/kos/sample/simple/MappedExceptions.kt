5c5bd5b43d361e07323e81439644d7e6ea99a71a
Miere Liniel Teixeira <miere00@gmail.com>
2022-03-06 23:04:38 +1100

    Using new ExceptionHandler semantics

--- src/main/kotlin/kos/sample/simple/MappedExceptions.kt
+++ src/main/kotlin/kos/sample/simple/MappedExceptions.kt
@@ -1,26 +1,22 @@
 package kos.sample.simple
 
 import injector.Exposed
-import io.vertx.core.http.HttpServerRequest
 import io.vertx.core.json.DecodeException
-import kos.api.ExceptionHandler
+import kos.api.MutableKosContext
+import kos.api.Plugin
 import kos.api.Response
-import org.slf4j.Logger
+import kos.core.exception.PredicateExceptionHandler
 
 @Exposed
-class MappedExceptions(private val logger: Logger): ExceptionHandler {
+class MappedExceptions: Plugin {
 
-    override fun handle(request: HttpServerRequest, cause: Throwable): Response {
-        return when (cause) {
-            is DecodeException -> Response.of(cause.message).statusCode(400)
-            is NotFound -> Response.NOT_FOUND
-            else -> internalServerError(cause)
-        }
+    private val exceptionHandler = PredicateExceptionHandler().apply {
+        add({ it is NotFound }) { _, _ -> Response.NOT_FOUND }
+        add({ it is DecodeException }) { _, cause -> Response.of(cause.message).statusCode(400) }
     }
 
-    private fun internalServerError(cause: Throwable): Response {
-        logger.error("Something terrible just happened", cause)
-        return Response.empty(500)
+    override fun configure(kosContext: MutableKosContext) {
+        kosContext.exceptionHandler = exceptionHandler
     }
 }
 
